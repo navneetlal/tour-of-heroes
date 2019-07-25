7acf2790166050f09f3d4403803faed3ba2d8965
Navneet Lal Gupta <navneetlalg@gmail.com>
2019-07-25 15:47:59 +0530

    initial commit

---
+++ src/app/hero.ts
@@ -0,0 +1,5 @@
+export class Hero {
+  author: string
+  content: string
+  title: string
+}
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { content: 'this is sample content for all author and title', author: 'Dr Nice', title: 'Angular Projects 1' },
+  { content: 'this is sample content for all author and title', author: 'Narco', title: 'Angular Projects 2' },
+  { content: 'this is sample content for all author and title', author: 'Bombasto', title: 'Angular Projects 3' },
+  { content: 'this is sample content for all author and title', author: 'Celeritas', title: 'Angular Projects 4' },
+  { content: 'this is sample content for all author and title', author: 'Magneta', title: 'Angular Projects 5' },
+  { content: 'this is sample content for all author and title', author: 'RubberMan', title: 'Angular Projects 6' },
+  { content: 'this is sample content for all author and title', author: 'Dynama', title: 'Angular Projects 7' },
+  { content: 'this is sample content for all author and title', author: 'Dr IQ', title: 'Angular Projects 8' },
+  { content: 'this is sample content for all author and title', author: 'Magma', title: 'Angular Projects 9' },
+  { content: 'this is sample content for all author and title', author: 'Tornado', title: 'Angular Projects 10' },
+];
